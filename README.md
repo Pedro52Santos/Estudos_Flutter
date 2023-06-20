69a42c3c57378845365c06958a11b5bfec0f9103
Pedro Santos <51803100+Pedro52Santos@users.noreply.github.com>
2023-06-20 18:08:27 -0400

    UPDATE

--- README.md
+++
@@ -1,2 +0,0 @@
-# Estudos_Flutter
-Neste repositório será armazenado o conteúdo gerado a partir da formação FLutter da Alura
---
+++ projetos/solo/solo/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
+
+//Components (SEPARAR DEPOIS)
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        IconButton(onPressed: () {}, icon: Icon(Icons.search_off_outlined))
+      ]),
+    );
+  }
+}
